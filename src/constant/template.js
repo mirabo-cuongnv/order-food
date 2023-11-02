44b9d3ef32aede0a9bf0a4677f43b2b7b4ff8471
hungpv@mirabo-global.com <hungpv>
2023-11-02 14:32:02 +0700

    delete file

--- src/constant/template.js
+++
@@ -1,2 +0,0 @@
-export const TEMPLATE_QA_CODE_V2 = 'compact2';
-export const TEMPLATE_QA_CODE = 'compact';
--- src/pages/Admin/components/order/OrderItem.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const OrderItem = ({ value }) => {
-  return <div className="font-mono font-semibold py-1 text-sm">- {value}</div>;
-};
-
-export default OrderItem;
